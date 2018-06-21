c5da95b5c1cf2e57c9023886d0f5b7682e1756a7
wen bo <ver3821@163.com>
2018-06-21 10:24:24 +0800

    test git communicate

---
+++ 1basic_types.js
@@ -0,0 +1,4 @@
+var names = 'typescript';
+var version = 2.7;
+var sentence = "hello,I'm " + names + ",now my version is " + version;
+console.log(sentence);
---
+++ 1basic_types.ts
@@ -0,0 +1,4 @@
+let names:string='typescript';
+let version:number=2.7;
+let sentence=`hello,I'm ${names},now my version is ${version}`;
+console.log(sentence)
